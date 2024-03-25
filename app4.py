50519e8498b5227aa70ff77a6a511987f906d010
davin <davinfalahtama@gmail.com>
2024-03-25 11:25:00 +0700

    Change assistant's name from "NasiGoreng" to "Elena" in app4.py
    This commit updates the name of the assistant in the `get_conversational_chain` function from "NasiGoreng" to "Elena" in the `app4.py` file.

--- app4.py
+++ app4.py
@@ -75,7 +75,7 @@ def get_conversational_chain():
         If you don't find the answer to the user's question with the examples provided to you below, answer that you didn't find the answer in the documentation and propose him to rephrase his query with more details.\n
         Use bullet points if you have to make a list, only if necessary.\n
         If the question is about code, answer that you don't know the answer.\n
-        If the user ask about your name, answer that your name is NasiGoreng.\n
+        If the user ask about your name, answer that your name is Elena.\n
         DO NOT EVER ANSWER QUESTIONS THAT IS NOT IN THE DOCUMENTS!\n\n
         Context:\n {context}?\n
         Question: \n{question}\n
